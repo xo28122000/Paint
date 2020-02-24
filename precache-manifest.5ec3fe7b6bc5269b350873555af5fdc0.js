fd5342d4a5342ee727632d87c52b1592e36879ec
xo28122000 <jainam.2000@yahoo.com>
2020-02-24 00:34:27 -0800

    init page

---
+++ precache-manifest.5ec3fe7b6bc5269b350873555af5fdc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08572551013833a608ef4e4720bef001",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28675d96e203becaed3a",
+    "url": "/static/css/main.197544a0.chunk.css"
+  },
+  {
+    "revision": "6ca32a8c09ec9b9fa108",
+    "url": "/static/js/2.3cf78579.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.3cf78579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28675d96e203becaed3a",
+    "url": "/static/js/main.e4bcf3d7.chunk.js"
+  },
+  {
+    "revision": "d7241f7207fc0c97304a",
+    "url": "/static/js/runtime-main.21581948.js"
+  }
+]);
